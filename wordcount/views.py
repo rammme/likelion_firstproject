19f2c361aa0c4afad11df0478af354c45cfac882
rammme <80208912+rammme@users.noreply.github.com>
2021-05-21 19:30:10 +0900

    Update views.py

--- wordcount/views.py
+++ wordcount/views.py
@@ -14,9 +14,8 @@ def result(request):
 
     for word in wordList: 
         if word in wordDict:
-            wordDict[word] += 1 # wordDict안에 단어가 있으면 1 증가
-        else:                   #처음 나오는 단어이면
-            wordDict[word] = 1  # 그 단어는 1로 초기화
+            wordDict[word] += 1 
+        else:                   
+            wordDict[word] = 1  
 
     return render(request, 'result.html', {'fulltext':sentence, 'count':len(wordList), 'wordDict':wordDict.items})
-    #result.html의 fulltext를 키 값으로, 값을 sentece로  / 
